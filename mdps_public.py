0d49b504d03976c4dfcb446b4c4e8476de7fffc0
NAVEENkumarsubraman <152303073+NAVEENkumarsubraman@users.noreply.github.com>
2023-11-28 21:02:22 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,24 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  8 21:01:15 2022
-
-@author: siddhardhan
-"""
-
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
-# loading the saved models
-
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 
-
-
-
-# sidebar for navigation
 with st.sidebar:
     
     selected = option_menu('Multiple Disease Prediction System',
@@ -29,7 +15,6 @@ with st.sidebar:
                           default_index=0)
     
     
-# Diabetes Prediction Page
 if (selected == 'Diabetes Prediction'):
     
     # page title
@@ -64,10 +49,9 @@ if (selected == 'Diabetes Prediction'):
         Age = st.text_input('Age of the Person')
     
     
-    # code for Prediction
     diab_diagnosis = ''
     
-    # creating a button for Prediction
+
     
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
